c08f2ccef09fb92f9032861e163fa1612a1ce1ea
devimalka <developerimalka@gmail.com>
2023-03-09 20:05:15 +0530

    migrate blog_posts migration

--- database/migrations/2023_03_09_142858_create_blog_posts_table.php
+++ database/migrations/2023_03_09_142858_create_blog_posts_table.php
@@ -15,6 +15,8 @@ return new class extends Migration
     {
         Schema::create('blog_posts', function (Blueprint $table) {
             $table->id();
+            $table->text('title');
+            $table->text('body');
             $table->timestamps();
         });
     }
