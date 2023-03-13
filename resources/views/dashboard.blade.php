f70d6832fb22f8a418950057a1c7900db09f076a
devimalka <developerimalka@gmail.com>
2023-03-13 22:53:48 +0530

    add blog comment and post centering

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,8 +1,41 @@
 <x-app-layout>
     <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Dashboard') }}
-        </h2>
+        <nav class="flex items-center justify-between flex-wrap bg-white p-6">
+            <div class="flex items-center flex-shrink-0 text-gray-900 mr-6">
+                <span class="font-semibold text-xl tracking-tight">My App</span>
+            </div>
+            <div class="block lg:hidden">
+                <button
+                    class="navbar-burger flex items-center px-3 py-2 border rounded text-gray-500 border-gray-600 hover:text-gray-900 hover:border-gray-900">
+                    <svg class="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                        <title>Menu</title>
+                        <path
+                            d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
+                    </svg>
+                </button>
+            </div>
+            <div class="navbar-menu hidden lg:flex lg:items-center lg:w-auto">
+                <a href="{{ route('blog.index') }}"
+                    class="block mt-4 lg:inline-block lg:mt-0 text-gray-900 hover:text-gray-900 mr-10">
+                    Home
+                </a>
+                @guest
+                <a href="{{ route('login') }}"
+                    class="block mt-4 lg:inline-block lg:mt-0 text-gray-900 hover:text-gray-900">
+                    Login
+                </a>
+                @else
+                <form method="POST" action="{{ route('logout') }}">
+                    @csrf
+                    <a href="{{ route('logout') }}" onclick="event.preventDefault();
+                                                this.closest('form').submit();"
+                        class="block mt-4 lg:inline-block lg:mt-0 text-gray-900 hover:text-gray-900">
+                        Logout
+                    </a>
+                </form>
+                @endguest
+            </div>
+        </nav>
     </x-slot>
 
     <div class="py-12">
--- resources/views/post/show.blade.php
+++ resources/views/post/show.blade.php
@@ -1,49 +1,63 @@
 @extends('layouts.base')
 
 @section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <h2>{{ ucfirst($post->title )}}</h2>
+                <p>{{ $post->body }}</p>
+            </div>
+        </div>
+    </div>
+
+    <div class="container">
+<div class="row">
+<div class="col-md-12">
+    @auth
+        @if (Auth::id() == $post->user_id)
+            <button type="button" class="btn btn-outline-danger"><a href="{{ route('blog.edit', $post->id) }}">Edit</a></button>
+            <form action="{{ route('blog.destroy', $post->id) }}" method='POST'>
+                @csrf
+                @METHOD('DELETE')
+                <button type="submit" class="btn btn-outline-danger">Delete</button>
+            </form>
+        @endif
+    @endauth
+
+    @foreach ($post->comments as $comment)
+        <div class="comment mt-4 text-justify float-left">
+        
+            <span>{{ $comment->created_at }}</span>
+            <br>
+            <p>{{ $comment->comment }}</p>
+        </div>
+    @endforeach
+</div>
+</div>
+</div>
+
+  
 
 
 <div class="container">
-    <div class="row">
-        <div class="col-md-12">
-            <h2>{{ $post->title }}</h2>
-            <p>{{ $post->body }}</p>
-        </div>
-    </div>
-</div>
-
-@foreach($post->comments as $comment)
-         
-            <div class="comment mt-4 text-justify float-left">
-                    <h4>Jhon Doe</h4>
-                    <span>{{$comment->created_at}}</span>
-                    <br>
-                    <p>{{ $comment->comment }}</p>
-                </div>
-            @endforeach
-
-
-
-@auth
-@if(Auth::id()==$post->user_id)
-<button type="button" class="btn btn-outline-danger"><a href="{{ route('blog.edit',$post->id) }}">Edit</a></button>
-<form action="{{ route('blog.destroy',$post->id) }}" method='POST'>
-@csrf
-@METHOD('DELETE')
-<button type="submit" class="btn btn-outline-danger">Delete</button>
-</form>
-@endif
-@endauth
-
-
-<div>
-    <form action="{{ route('blog.comment.store',$post) }}" method='POST'>
-        @csrf
+<div class="row">
+<div class="col-md-12">
+        <form action="{{ route('blog.comment.store', $post) }}" method='POST'>
+  @csrf
+  <div class="form-inline">
+    <label for="comment" class="form-label">Comment</label>
     <input type="text" name='comment'>
-    <input type="submit">
+    <button type="submit" class="btn btn-primary">Reply</button>
+  </div>
+ 
+</form>
 
-    </form>
+
 </div>
 </div>
+</div>
+
+
+ 
+    
 @endsection
-
